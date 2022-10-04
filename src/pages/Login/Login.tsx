f05db8b1fe4be063596c0da770a9106016bb9869
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-10-04 09:18:37 +0530

    Signup page added

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -19,65 +19,66 @@ import {
   ViewIcon,
   ViewOffIcon,
   ArrowForwardIcon,
+  AtSignIcon,
 } from "@chakra-ui/icons";
 import { Link } from "react-router-dom";
 
 function Login() {
-  const [show, setShow] = React.useState(false);
-  const handleClick = () => setShow(!show);
+  const [showPassword, setShowPassword] = React.useState(false);
+  const handleViewPassword = () => setShowPassword(!showPassword);
   return (
-    <Box>
+    <Box bg="gray.50">
       <Flex alignItems="center" justifyContent="space-evenly" height="100vh">
-        <Stack direction="column" spacing={3}>
-          <Heading >
-            Log in to your account
-          </Heading>
-          <HStack spacing="1" justify="center">
-            <Text color="muted">Don't have an account?</Text>
-            <Link to="/signup">
-            <Button variant="link" colorScheme="blue">
-              Sign up
+        <Box boxShadow="lg" p="6" rounded="md" bg="white">
+          <Stack direction="column" spacing={3}>
+            <Heading>Log in to your account</Heading>
+            <HStack spacing="1" justify="center">
+              <Text color="muted">Don't have an account?</Text>
+              <Link to="/signup">
+                <Button variant="link" colorScheme="blue">
+                  Sign up
+                </Button>
+              </Link>
+            </HStack>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<AtSignIcon color="gray.300" />}
+              />
+              <Input type="email" placeholder="Email" />
+            </InputGroup>
+
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<LockIcon color="gray.300" />}
+              />
+              <Input
+                pr="4.5rem"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleViewPassword}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
+            <Button rightIcon={<ArrowForwardIcon />} colorScheme="teal">
+              Log In
             </Button>
-            </Link>
-          </HStack>
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<PhoneIcon color="gray.300" />}
-            />
-            <Input type="email" placeholder="Phone number" />
-          </InputGroup>
 
-          <InputGroup>
-            <InputLeftElement
-              pointerEvents="none"
-              children={<LockIcon color="gray.300" />}
-            />
-            <Input
-              pr="4.5rem"
-              type={show ? "text" : "password"}
-              placeholder="Enter password"
-            />
-            <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm" onClick={handleClick}>
-                {show ? <ViewIcon /> : <ViewOffIcon />}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-
-          <Button rightIcon={<ArrowForwardIcon />} colorScheme="teal">
-            Call us
-          </Button>
-
-          <HStack>
-            <Divider />
-            <Text fontSize="sm" whiteSpace="nowrap" color="muted">
-              or continue with
-            </Text>
-            <Divider />
-          </HStack>
-          <Button width="full">Sign in with Google</Button>
-        </Stack>
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                or continue with
+              </Text>
+              <Divider />
+            </HStack>
+            <Button width="full">Sign in with Google</Button>
+          </Stack>
+        </Box>
       </Flex>
     </Box>
   );
--- src/pages/Signup/Signup.tsx
+++ src/pages/Signup/Signup.tsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+  Radio,
+  RadioGroup,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  PhoneIcon,
+  LockIcon,
+  ViewIcon,
+  ViewOffIcon,
+  ArrowForwardIcon,
+  AddIcon,
+  AtSignIcon,
+} from "@chakra-ui/icons";
+import { Link } from "react-router-dom";
 
 function Signup() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleViewPassword = () => setShowPassword(!showPassword);
   return (
-    <div>Signup</div>
-  )
+    <Box bg='gray.50'>
+      <Flex alignItems="center" justifyContent="space-evenly" height="100vh">
+        <Box boxShadow='lg' p='6' rounded='md' bg='white'>
+          <Stack direction="column" spacing={3}>
+            <Heading>Create your account</Heading>
+            <HStack spacing="1" justify="center">
+              <Text color="muted">Already have an account?</Text>
+              <Link to="/login">
+                <Button variant="link" colorScheme="blue">
+                  Log in
+                </Button>
+              </Link>
+            </HStack>
+
+            <Center>
+              <RadioGroup defaultValue="1">
+                <Stack spacing={5} direction="row">
+                  <Radio colorScheme="teal" value="1">
+                    Bidder
+                  </Radio>
+                  <Radio colorScheme="teal" value="2">
+                    Seller
+                  </Radio>
+                </Stack>
+              </RadioGroup>
+            </Center>
+
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<AtSignIcon color="gray.300" />}
+              />
+              <Input type="email" placeholder="Email" />
+            </InputGroup>
+
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<LockIcon color="gray.300" />}
+              />
+              <Input
+                pr="4.5rem"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleViewPassword}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<LockIcon color="gray.300" />}
+              />
+              <Input
+                pr="4.5rem"
+                type={showPassword ? "text" : "password"}
+                placeholder="Confirm password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handleViewPassword}>
+                  {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+
+            <Button rightIcon={<AddIcon />} colorScheme="teal">
+              Sign Up
+            </Button>
+
+            <HStack>
+              <Divider />
+              <Text fontSize="sm" whiteSpace="nowrap" color="muted">
+                or continue with
+              </Text>
+              <Divider />
+            </HStack>
+            <Button width="full">Sign up with Google</Button>
+          </Stack>
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
 
-export default Signup
+export default Signup;
