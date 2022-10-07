17a3a7975c0ab1e1501b5019f2bff2049fa95cfe
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-10-07 22:44:17 +0530

    page404 added

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,19 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Registration from "./pages/Registration/Registration";
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="*" element={<PageNotFound />} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/registration" element={<Registration />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
