3cb84e3f62cd219a56ed9bf818ffa06e0bc763cf
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-10-16 16:06:51 +0530

    added gauth and logout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,10 @@
 import React from "react";
-import { Box, Button, Flex, HStack, Spacer, useColorModeValue } from "@chakra-ui/react";
+import { Box, Button, Flex, HStack, Spacer, useColorModeValue, useToast } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import ThemeSwitcher from "./ThemeSwitcher";
+import { signOut } from "firebase/auth";
+import { auth } from "../../utils/firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 const NavItems = [
   {
@@ -17,13 +20,37 @@ const NavItems = [
 ];
 
 function Navbar() {
+  const navigate = useNavigate();
+  const toast = useToast()
+
     const navbarBg = useColorModeValue("gray.200", "whiteAlpha.50")
+
+    const logoutHandler = () => {
+      signOut(auth).then(() => {
+        // Sign-out successful.
+        toast({
+          title: 'Logout successfull.',
+          status: 'success',
+          duration: 2000,
+          isClosable: false,
+        })
+        navigate("/")
+      }).catch((error) => {
+        toast({
+          title: error.message,
+          description: error.code,
+          status: 'error',
+          duration: 2000,
+          isClosable: false,
+        })
+      });
+    }
   return (
     <Box my={2} mx={6} bg={navbarBg} borderRadius="md">
       <Flex py={2} px={4}>
         <HStack spacing="24px">
           {NavItems.map((item) => (
-            <Link to={item.href}>
+            <Link to={item.href} key={item.navItem}>
               <Button colorScheme="teal" variant="ghost">
                 {item.navItem}
               </Button>
@@ -33,7 +60,7 @@ function Navbar() {
         <Spacer />
         <HStack spacing="24px">
             <ThemeSwitcher/>
-          <Button colorScheme="red" variant="solid">
+          <Button colorScheme="red" variant="solid" onClick={logoutHandler}>
             Log Out
           </Button>
         </HStack>
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -11,21 +11,97 @@ import {
   InputRightElement,
   Stack,
   Text,
+  useToast,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import {
-  PhoneIcon,
   LockIcon,
   ViewIcon,
   ViewOffIcon,
   ArrowForwardIcon,
   AtSignIcon,
 } from "@chakra-ui/icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { auth } from "../../utils/firebaseConfig";
 
 function Login() {
-  const [showPassword, setShowPassword] = React.useState(false);
+  const toast = useToast()
+  const navigate = useNavigate();
+  const [userSignInInfo, setUserSignInInfo] = useState({
+    email: "",
+    password: "",
+  });
+  const [showPassword, setShowPassword] = useState(false);
   const handleViewPassword = () => setShowPassword(!showPassword);
+
+  const handleInput = (event: any) => {
+    setUserSignInInfo({
+      ...userSignInInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const loginUserUsingCred = (event:any) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(
+      auth,
+      userSignInInfo.email,
+      userSignInInfo.password
+    )
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        toast({
+          title: 'Login successfull.',
+          status: 'success',
+          duration: 3000,
+          isClosable: false,
+        })
+        
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  const gProvider = new GoogleAuthProvider();
+
+  const googleSignin = (event:any) => {
+    event.preventDefault()
+    signInWithPopup(auth, gProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        if (credential!==null){
+          const token = credential.accessToken;
+        }
+        toast({
+          title: `Login for ${result.user.email} is successfull.`,
+          // description: "We've created your account for you.",
+          status: 'success',
+          duration: 2000,
+          isClosable: false,
+        })
+        navigate("/home")
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        toast({
+          title: error.message,
+          description: error.code,
+          status: 'error',
+          duration: 2000,
+          isClosable: false,
+        })
+      });
+  };
+
   return (
     <Box bg="gray.50">
       <Flex alignItems="center" justifyContent="space-evenly" height="100vh">
@@ -40,12 +116,19 @@ function Login() {
                 </Button>
               </Link>
             </HStack>
+
             <InputGroup>
               <InputLeftElement
                 pointerEvents="none"
                 children={<AtSignIcon color="gray.300" />}
               />
-              <Input type="email" placeholder="Email" />
+              <Input
+                type="email"
+                placeholder="Email"
+                name="email"
+                defaultValue={userSignInInfo.email}
+                onChange={handleInput}
+              />
             </InputGroup>
 
             <InputGroup>
@@ -57,6 +140,8 @@ function Login() {
                 pr="4.5rem"
                 type={showPassword ? "text" : "password"}
                 placeholder="Enter password"
+                defaultValue={userSignInInfo.password}
+                onChange={handleInput}
               />
               <InputRightElement width="4.5rem">
                 <Button h="1.75rem" size="sm" onClick={handleViewPassword}>
@@ -65,7 +150,11 @@ function Login() {
               </InputRightElement>
             </InputGroup>
 
-            <Button rightIcon={<ArrowForwardIcon />} colorScheme="teal">
+            <Button
+              rightIcon={<ArrowForwardIcon />}
+              colorScheme="teal"
+              onClick={loginUserUsingCred}
+            >
               Log In
             </Button>
 
@@ -76,7 +165,7 @@ function Login() {
               </Text>
               <Divider />
             </HStack>
-            <Button width="full">Sign in with Google</Button>
+            <Button width="full" onClick={googleSignin}>Sign in with Google</Button>
           </Stack>
         </Box>
       </Flex>
