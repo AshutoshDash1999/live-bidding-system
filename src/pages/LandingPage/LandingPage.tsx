7c9d07c955991c76f1e24f66da91a382e314c49a
Dinesh Rout <rddinesh779@gmail.com>
2022-10-19 00:47:43 +0530

    refactor: formatting

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -6,31 +6,30 @@ import {
   Flex,
   Heading,
   Highlight,
-} from "@chakra-ui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 function LandingPage() {
   return (
     <Box>
-      <Flex alignItems="center" justifyContent="space-evenly" height="100vh">
+      <Flex alignItems='center' justifyContent='space-evenly' height='100vh'>
         <Box>
-          <Heading as="h1" size="3xl" mb="2rem">
+          <Heading as='h1' size='3xl' mb='2rem'>
             <Highlight
-              query={["Bidding", "easy"]}
-              styles={{ px: "6", py: "1", rounded: "full", bg: "teal.100" }}
+              query={['Bidding', 'easy']}
+              styles={{ px: '6', py: '1', rounded: 'full', bg: 'teal.100' }}
             >
               Bidding made Easy
             </Highlight>
           </Heading>
 
           <Center>
-            <ButtonGroup gap="2">
-              <Link to="/signup">
-                <Button colorScheme="teal">Sign Up</Button>
+            <ButtonGroup gap='2'>
+              <Link to='/signup'>
+                <Button colorScheme='teal'>Sign Up</Button>
               </Link>
-              <Link to="/login">
-                <Button colorScheme="teal">Log in</Button>
+              <Link to='/login'>
+                <Button colorScheme='teal'>Log in</Button>
               </Link>
             </ButtonGroup>
           </Center>
