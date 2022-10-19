ed77da6df3f2cc2598db7dce33177a95b42af066
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-10-19 08:47:48 +0530

    display name added in nav

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,20 @@
-import React from "react";
-import { Box, Button, Flex, HStack, Spacer, useColorModeValue, useToast } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Spacer,
+  useColorModeValue,
+  useToast,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import ThemeSwitcher from "./ThemeSwitcher";
 import { signOut } from "firebase/auth";
 import { auth } from "../../utils/firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const NavItems = [
   {
@@ -21,30 +31,43 @@ const NavItems = [
 
 function Navbar() {
   const navigate = useNavigate();
-  const toast = useToast()
+  const toast = useToast();
+  const [user, loading, error] = useAuthState(auth);
+  const [showUserName, setUserName] = useState<string>("")
+  const [showUserPhoto, setUserPhoto] = useState<string>("")
+  useEffect(()=>{
+    if (user){
+      setUserName(user.displayName as string)
+      setUserPhoto(user.photoURL as string)
+    }
+  }, [user])
 
-    const navbarBg = useColorModeValue("gray.200", "whiteAlpha.50")
 
-    const logoutHandler = () => {
-      signOut(auth).then(() => {
+
+  const navbarBg = useColorModeValue("gray.200", "whiteAlpha.50");
+
+  const logoutHandler = () => {
+    signOut(auth)
+      .then(() => {
         // Sign-out successful.
         toast({
-          title: 'Logout successfull.',
-          status: 'success',
+          title: "Logout successfull.",
+          status: "success",
           duration: 2000,
           isClosable: false,
-        })
-        navigate("/")
-      }).catch((error) => {
+        });
+        navigate("/");
+      })
+      .catch((error) => {
         toast({
           title: error.message,
           description: error.code,
-          status: 'error',
+          status: "error",
           duration: 2000,
           isClosable: false,
-        })
+        });
       });
-    }
+  };
   return (
     <Box my={2} mx={6} bg={navbarBg} borderRadius="md">
       <Flex py={2} px={4}>
@@ -59,7 +82,10 @@ function Navbar() {
         </HStack>
         <Spacer />
         <HStack spacing="24px">
-            <ThemeSwitcher/>
+          <Button colorScheme="teal" variant="ghost">
+            Welcome, {showUserName} <Avatar name={showUserName} ml="2" size='sm' src={showUserPhoto} />
+          </Button>
+          <ThemeSwitcher />
           <Button colorScheme="red" variant="solid" onClick={logoutHandler}>
             Log Out
           </Button>
