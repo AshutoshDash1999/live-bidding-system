101daaec2a70b7b9fc4fefba24997f171b1df72f
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-10-31 01:27:36 +0530

    get item from fb

--- src/pages/SellerDashboard/SellerDashboard.tsx
+++ src/pages/SellerDashboard/SellerDashboard.tsx
@@ -13,22 +13,101 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  NumberInput,
-  NumberInputField,
+  Stack,
   Textarea,
   useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { db } from "../../utils/firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  onSnapshot,
+} from "firebase/firestore";
+import ItemCard from "../../components/ItemCard/ItemCard";
 
 function SellerDashboard() {
+  const toast = useToast();
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [itemInfo, setItemInfo] = useState({
+    itemName: "",
+    itemBasePrice: "",
+    itemDesc: "",
+    auctionEndTime: "",
+    itemPhoto: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  const [itemDataCollection, setItemDataCollection] = useState([]);
+
+  const handleInput = (event: any) => {
+    setItemInfo({
+      ...itemInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  async function itemPublishClickHandler(e: any) {
+    e.preventDefault();
+    console.log(itemInfo);
+    setIsLoading(true);
+    try {
+      const docRef = await addDoc(collection(db, "itemData"), {
+        itemName: itemInfo.itemName,
+        itemBasePrice: itemInfo.itemBasePrice,
+        itemDesc: itemInfo.itemDesc,
+        auctionEndTime: itemInfo.auctionEndTime,
+      });
+      // console.log("Document written with ID: ", docRef.id);
+      setIsLoading(false);
+      toast({
+        title: "Your item is online now",
+        status: "success",
+        duration: 2000,
+        isClosable: false,
+      });
+    } catch (e) {
+      console.error("Error adding item: ", e);
+      setIsLoading(false);
+      toast({
+        title: "Error adding item",
+        description: `${e}`,
+        status: "error",
+        duration: 2000,
+        isClosable: false,
+      });
+    }
+  }
+
+  useEffect(() => {
+    const querySnap = query(collection(db, "itemData"));
+    const unsub = onSnapshot(querySnap, (querySnapshot) => {
+      let itemDataArray: any = [];
+      querySnapshot.forEach((doc) => {
+        console.log(doc.data());
+        itemDataArray.push(doc.data());
+      });
+      setItemDataCollection(itemDataArray);
+    });
+    return () => unsub();
+  }, []);
 
   return (
     <Box>
       <Navbar />
       <Box my={2} mx={6} borderRadius="md">
         <Button onClick={onOpen}>Open Modal</Button>
+      </Box>
+
+      <Box my={2} mx={6} borderRadius="md">
+        <Stack spacing={8} direction='row'>
+          {itemDataCollection.map((item) => (
+            <ItemCard />
+          ))}
+        </Stack>
       </Box>
 
       <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
@@ -39,19 +118,43 @@ function SellerDashboard() {
           <ModalBody pb={6}>
             <FormControl isRequired marginBottom={4}>
               <FormLabel>Item Name</FormLabel>
-              <Input type="email" />
+              <Input
+                type="text"
+                name="itemName"
+                defaultValue={itemInfo.itemName}
+                onChange={handleInput}
+              />
             </FormControl>
 
             <FormControl isRequired marginBottom={4}>
               <FormLabel>Item Base Price</FormLabel>
-              <NumberInput>
-                <NumberInputField />
-              </NumberInput>
+              <Input
+                name="itemBasePrice"
+                defaultValue={itemInfo.itemBasePrice}
+                onChange={handleInput}
+              ></Input>
             </FormControl>
-            
+
             <FormControl isRequired marginBottom={4}>
               <FormLabel>Item Description</FormLabel>
-              <Textarea placeholder='' />
+              <Textarea
+                placeholder=""
+                name="itemDesc"
+                defaultValue={itemInfo.itemDesc}
+                onChange={handleInput}
+              />
+            </FormControl>
+
+            <FormControl isRequired marginBottom={4}>
+              <FormLabel>Add Image</FormLabel>
+              <Input
+                pt="1"
+                name="itemPhoto"
+                type="file"
+                accept="image/x-png,image/gif,image/jpeg"
+                defaultValue={itemInfo.itemPhoto}
+                onChange={handleInput}
+              />
             </FormControl>
 
             <FormControl isRequired marginBottom={4}>
@@ -60,13 +163,21 @@ function SellerDashboard() {
                 placeholder="Select Date and Time"
                 size="md"
                 type="datetime-local"
+                name="auctionEndTime"
+                defaultValue={itemInfo.auctionEndTime}
+                onChange={handleInput}
               />
             </FormControl>
-
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr={3}>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={itemPublishClickHandler}
+              isLoading={isLoading}
+              loadingText="Publishing..."
+            >
               Publish
             </Button>
             <Button onClick={onClose}>Cancel</Button>
