1fd136034cfc47ae505313c09ae2a199c42a1b1e
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-11-15 19:13:33 +0530

    ui update and code refractor with bug fixes

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,4 +1,8 @@
 import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
   Box,
   Button,
   Center,
@@ -23,11 +27,19 @@ import Navbar from "../../components/Navbar/Navbar";
 import ProductImg from "../../assets/product-img.jpg";
 import { CheckIcon } from "@chakra-ui/icons";
 import { useParams } from "react-router-dom";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, DocumentData, getDoc } from "firebase/firestore";
 import { db } from "../../utils/firebaseConfig";
-import dayjs from 'dayjs';
-import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
 dayjs.extend(relativeTime);
+
+interface ProductDataType {
+  itemId:string,
+  itemPrice:string,
+  itemName:string,
+  itemDesc:string,
+  auctionTimeLeft:string
+}
 
 function ProductPage() {
   const toast = useToast();
@@ -35,12 +47,12 @@ function ProductPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [newBidderPrice, setNewBidderPrice] = useState("");
   const [prevBiddedPrice, setPrevBiddedPrice] = useState(0);
-  const [productData, setProductData] = useState({});
+  const [productData, setProductData] = useState({} as ProductDataType);
 
   let { productID } = useParams();
 
   const priceUpdateHandler = () => {
-    if (parseInt(newBidderPrice) > productData?.itemPrice) {
+    if (parseInt(newBidderPrice) > parseInt(productData?.itemPrice)) {
       if (parseInt(newBidderPrice) > prevBiddedPrice) {
         toast({
           title: "Bidding successfull.",
@@ -71,13 +83,12 @@ function ProductPage() {
 
   useEffect(() => {
     const fetchProductData = async () => {
-      // const docRef = doc(db, "itemData", "b3cf5d6f5dda584d");
       try {
         const docRef = doc(db, "itemData", `${productID?.toString()}`);
         const docSnap = await getDoc(docRef);
         if (docSnap.exists()) {
           console.log(docSnap.data());
-          setProductData(docSnap.data());
+          setProductData(docSnap.data() as ProductDataType);
         } else {
           // doc.data() will be undefined in this case
           toast({
@@ -132,65 +143,73 @@ function ProductPage() {
             <Spacer />
             <Box>
               <Text color={"gray.600"}>
-                Product ID: {productData?.itemId.toUpperCase()}
+                Product ID: {productData!.itemId.toUpperCase()}
               </Text>
 
               <Heading mb={2} as="h2" size="2xl">
-                {productData?.itemName}
+                {productData!.itemName}
               </Heading>
-              <HStack spacing="24px">
-                <Box background="gray.200" p={4} borderRadius={"md"}>
-                  <Text fontSize="lg">Auction ending in</Text>
-                  <Text fontSize="2xl" fontWeight="bold">{dayjs(productData?.auctionTimeLeft).fromNow()}</Text>
-                </Box>
-                <Box>
-                  <Text fontSize="lg">
-                    Auction Price: ₹
-                    <span>
-                      {productData?.itemPrice.toLocaleString("en-IN")}
-                    </span>
-                  </Text>
-                  <InputGroup size="lg">
-                    <InputLeftElement
-                      pointerEvents="none"
-                      color="gray.300"
-                      fontSize="1.2em"
-                      children="₹"
-                    />
-                    <Input
-                      onChange={(e) =>
-                        setNewBidderPrice(e.target.value as string)
-                      }
-                      size="lg"
-                      pr="4.5rem"
-                      placeholder="Bid your price"
-                    />
-                    <InputRightElement width="4.5rem">
-                      <Button
-                        mr="0.5rem"
-                        h="2rem"
+              {dayjs(productData!.auctionTimeLeft).fromNow().includes("ago") ? (
+                <Alert status="error" borderRadius="md" variant='left-accent'>
+                  <AlertIcon />
+                  <AlertTitle>Timer expired!</AlertTitle>
+                  <AlertDescription>
+                    Please check other auctions in our platform.
+                  </AlertDescription>
+                </Alert>
+              ) : (
+                <HStack spacing="24px">
+                  <Box background="gray.200" p={4} borderRadius={"md"}>
+                    <Text fontSize="lg">Auction ending in</Text>
+                    <Text fontSize="2xl" fontWeight="bold">
+                      {dayjs(productData!.auctionTimeLeft).fromNow()}
+                    </Text>
+                  </Box>
+                  <Box>
+                    <Text fontSize="lg">
+                      Auction Price: ₹
+                      <span>
+                        {productData!.itemPrice.toLocaleString()}
+                      </span>
+                    </Text>
+                    <InputGroup size="lg">
+                      <InputLeftElement
+                        pointerEvents="none"
+                        color="gray.300"
+                        fontSize="1.2em"
+                        children="₹"
+                      />
+                      <Input
+                        onChange={(e) =>
+                          setNewBidderPrice(e.target.value as string)
+                        }
                         size="lg"
-                        onClick={priceUpdateHandler}
-                      >
-                        Bid
-                      </Button>
-                    </InputRightElement>
-                  </InputGroup>
-                </Box>
-              </HStack>
-              <Box
-                p={4}
-                my={4}
-                background={"green.100"}
-                borderRadius={"lg"}
-                borderBottom="8px"
-                borderColor={"green.400"}
-              >
-                <Text fontSize="lg">
+                        pr="4.5rem"
+                        placeholder="Bid your price"
+                      />
+                      <InputRightElement width="4.5rem">
+                        <Button
+                          mr="0.5rem"
+                          h="2rem"
+                          size="lg"
+                          onClick={priceUpdateHandler}
+                        >
+                          Bid
+                        </Button>
+                      </InputRightElement>
+                    </InputGroup>
+                  </Box>
+                </HStack>
+              )}
+
+              <Alert my="2" status="success" borderRadius="md" variant='left-accent'>
+                  <AlertDescription>
+                  <Text fontSize="lg">
                   Your last bidded Price: ₹
                   <span>{prevBiddedPrice.toLocaleString()}</span>
                 </Text>
-              </Box>
+                  </AlertDescription>
+                </Alert>
               <Box>
                 <Heading fontSize="md">Product Features</Heading>
                 <List spacing={3}>
