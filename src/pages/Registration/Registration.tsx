b28026f44af1b2e9f836d8ab2ffe0ac29bb2776d
Dinesh Rout <rddinesh779@gmail.com>
2022-12-30 21:58:23 +0530

    feat: registration form validation

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -4,6 +4,7 @@ import {
   Button,
   Flex,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
@@ -18,6 +19,7 @@ import { auth, db } from '../../utils/firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
 
 function Registration() {
   const toast = useToast();
@@ -32,17 +34,42 @@ function Registration() {
     yearlyIncome: '',
   });
   const [isLoading, setIsLoading] = useState(false);
-  const [userEmail, setUserEmail] = useState("")
+  const [userEmail, setUserEmail] = useState('');
   const [user, loading, error] = useAuthState(auth);
 
-  useEffect(()=>{
-    if (user){
-      setUserEmail(user.email as string)
-    }
-  }, [user])
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+    setValue,
+  } = useForm({
+    defaultValues: {
+      fname: '',
+      lname: '',
+      mobile: '',
+      email: userEmail ? userEmail : '',
+      bank: '',
+    },
+  });
 
-  async function saveRegistrationInfo(e: any) {
-    e.preventDefault();
+  useEffect(() => {
+    if (user) {
+      setUserEmail(user.email as string);
+    }
+  }, [user]);
+
+  // set user email dynamically
+  useEffect(() => {
+    if (userEmail) {
+      setValue('email', userEmail);
+    }
+  }, [userEmail]);
+
+  async function onSubmit(values: any) {
+    // console.log(values);
+
+    // console.log('submitted!');
+
     setIsLoading(true);
     try {
       const docRef = await addDoc(collection(db, 'userData'), {
@@ -63,7 +90,6 @@ function Registration() {
         isClosable: false,
       });
       navigate('/home');
-
     } catch (e) {
       console.error('Error adding document: ', e);
       setIsLoading(false);
@@ -77,120 +103,148 @@ function Registration() {
     }
   }
 
-  const handleInput = (event: any) => {
-    setRegisterUserInfo({
-      ...registerUserInfo,
-      [event.target.name]: event.target.value,
-    });
-  };
-
   return (
     <Box>
       <Flex alignItems='center' justifyContent='center' height='100vh'>
-        <Box width='30%'>
-          <Stack spacing={3}>
-            <FormControl isRequired>
-              <RadioGroup
-                name='role'
-                value={registerUserInfo.role}
-                onChange={(value) =>
-                  setRegisterUserInfo({
-                    ...registerUserInfo,
-                    role: value,
-                  })
-                }
-              >
-                <Stack direction={'row'}>
-                  <FormLabel>Choose your role:</FormLabel>
-                  <Radio name='role' id='bidder' value='bidder'>
-                    Bidder
-                  </Radio>
-                  <Radio name='role' id='seller' value='seller'>
-                    Seller
-                  </Radio>
-                </Stack>
-              </RadioGroup>
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>First name</FormLabel>
-              <Input
-                placeholder='Jane'
-                name='fname'
-                defaultValue={registerUserInfo.fname}
-                onChange={handleInput}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Last name</FormLabel>
-              <Input
-                placeholder='Doe'
-                name='lname'
-                defaultValue={registerUserInfo.lname}
-                onChange={handleInput}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Mobile Number</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents='none'
-                  children={<PhoneIcon color='gray.300' />}
-                />
+        <Box width='100%' maxW={'md'} mx='4'>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Stack spacing={3}>
+              <FormControl isRequired>
+                <RadioGroup
+                  name='role'
+                  value={registerUserInfo.role}
+                  onChange={(value) =>
+                    setRegisterUserInfo({
+                      ...registerUserInfo,
+                      role: value,
+                    })
+                  }
+                >
+                  <Stack direction={'row'}>
+                    <FormLabel>Choose your role:</FormLabel>
+                    <Radio name='role' id='bidder' value='bidder'>
+                      Bidder
+                    </Radio>
+                    <Radio name='role' id='seller' value='seller'>
+                      Seller
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+
+              {/* first name */}
+              <FormControl isInvalid={errors.fname}>
+                <FormLabel htmlFor='fname'>First name</FormLabel>
                 <Input
-                  type='tel'
-                  placeholder='Phone number'
-                  name='mobileNumber'
-                  defaultValue={registerUserInfo.mobileNumber}
-                  onChange={handleInput}
+                  id='fname'
+                  placeholder='John'
+                  {...register('fname', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 4,
+                      message: 'Minimum length should be 4',
+                    },
+                  })}
                 />
-              </InputGroup>
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Mail Id</FormLabel>
-              <InputGroup>
-                <InputLeftElement
-                  pointerEvents='none'
-                  children={<AtSignIcon color='gray.300' />}
-                />
+                <FormErrorMessage>
+                  {errors.fname && errors.fname.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              {/* last name */}
+              <FormControl isInvalid={errors.lname}>
+                <FormLabel htmlFor='lname'>Last name</FormLabel>
                 <Input
-                  type='email'
-                  name='mailId'
-                  value={userEmail}
-                  disabled
+                  id='lname'
+                  placeholder='Doe'
+                  {...register('lname', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 4,
+                      message: 'Minimum length should be 4',
+                    },
+                  })}
                 />
-              </InputGroup>
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Bank Account Number</FormLabel>
-              <Input
-                placeholder='xxx-xxx-xxx-xxx'
-                name='bankAccountNo'
-                defaultValue={registerUserInfo.bankAccountNo}
-                onChange={handleInput}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Yearly Income</FormLabel>
-              <Input
-                placeholder=''
-                name='yearlyIncome'
-                defaultValue={registerUserInfo.yearlyIncome}
-                onChange={handleInput}
-              />
-            </FormControl>
-            <Flex alignItems='center' justifyContent='space-between'>
-              {/* <Button width="10rem" onClick={resetFormHandler}>Reset</Button> */}
-              <Button
-                width='100%'
-                colorScheme='blue'
-                onClick={saveRegistrationInfo}
-                isLoading={isLoading}
-                loadingText='Saving'
-              >
-                Save
-              </Button>
-            </Flex>
-          </Stack>
+                <FormErrorMessage>
+                  {errors.lname && errors.lname.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              {/* mobile number */}
+
+              <FormControl isInvalid={errors.mobile}>
+                <FormLabel htmlFor='mobile'>Mobile Number</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents='none'
+                    children={<PhoneIcon color='gray.300' />}
+                  />
+                  <Input
+                    id='mobile'
+                    placeholder='9090124523'
+                    {...register('mobile', {
+                      required: 'This is required',
+                      minLength: {
+                        value: 10,
+                        message: 'Minimum length should be 10',
+                      },
+                    })}
+                  />
+                </InputGroup>
+                <FormErrorMessage>
+                  {errors.mobile && errors.mobile.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              {/* email */}
+              <FormControl>
+                <FormLabel htmlFor='email'>Mail Id</FormLabel>
+                <InputGroup>
+                  <InputLeftElement
+                    pointerEvents='none'
+                    children={<AtSignIcon color='gray.300' />}
+                  />
+                  <Input
+                    id='email'
+                    {...register('email', {
+                      disabled: true,
+                      value: userEmail,
+                    })}
+                  />
+                </InputGroup>
+              </FormControl>
+
+              {/* bank */}
+              <FormControl isInvalid={errors.bank}>
+                <FormLabel htmlFor='bank'>Bank Account Number</FormLabel>
+                <Input
+                  placeholder='xxx-xxx-xxx-xxx'
+                  id='bank'
+                  {...register('bank', {
+                    required: 'This is required',
+                    minLength: {
+                      value: 10,
+                      message: 'Minimum length should be 10',
+                    },
+                  })}
+                />
+                <FormErrorMessage>
+                  {errors.bank && errors.bank.message}
+                </FormErrorMessage>
+              </FormControl>
+              <Flex alignItems='center' justifyContent='space-between'>
+                <Button
+                  width='100%'
+                  colorScheme='blue'
+                  type='submit'
+                  isLoading={isLoading}
+                  loadingText='Saving'
+                >
+                  Save
+                </Button>
+              </Flex>
+            </Stack>
+          </form>
         </Box>
       </Flex>
     </Box>
