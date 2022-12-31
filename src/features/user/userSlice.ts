7e66bfbe39186a6932beb8fb3db1becec22d097d
Dinesh Rout <rddinesh779@gmail.com>
2022-12-31 13:22:19 +0530

    fix: default role fixed

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,36 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
-import type { PayloadAction } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
 
 export interface UserState {
-    userName: string,
-    userEmail: string,
-    userRole: string
+  userName: string;
+  userEmail: string;
+  userRole: string;
 }
 
 const initialState: UserState = {
-    userName: "",
-    userEmail: "",
-    userRole: "bidder"
-}
+  userName: '',
+  userEmail: '',
+  userRole: '',
+};
 
 export const userInfoSlice = createSlice({
-    name:'currentUserStore',
-    initialState,
-    reducers:{
-        updateUserName: (state, action: PayloadAction<string>) => {
-            state.userName = action.payload
-        },
-        updateUserEmail: (state, action: PayloadAction<string>) => {
-            state.userEmail = action.payload
-        },
-        updateUserRole: (state, action: PayloadAction<string>) => {
-            state.userRole = action.payload
-        }
-    }
-})
+  name: 'currentUserStore',
+  initialState,
+  reducers: {
+    updateUserName: (state, action: PayloadAction<string>) => {
+      state.userName = action.payload;
+    },
+    updateUserEmail: (state, action: PayloadAction<string>) => {
+      state.userEmail = action.payload;
+    },
+    updateUserRole: (state, action: PayloadAction<string>) => {
+      state.userRole = action.payload;
+    },
+  },
+});
 
 // Action creators are generated for each case reducer function
-export const { updateUserName, updateUserEmail, updateUserRole } = userInfoSlice.actions
+export const { updateUserName, updateUserEmail, updateUserRole } =
+  userInfoSlice.actions;
 
-export default userInfoSlice.reducer
-
+export default userInfoSlice.reducer;
