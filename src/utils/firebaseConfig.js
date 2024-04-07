7a975b8b654fa758fedc60c300c7bf7d4d5ab07c
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-04-07 20:08:01 +0530

    chore: firebase env variables update

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_API_KEY,
-  authDomain: NEXT_PUBLIC_AUTHDOMAIN,
-  projectId: NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_MESSAGING_SENDERID,
-  appId: NEXT_PUBLIC_APPID,
-  measurementId: NEXT_PUBLIC_MEASUREMENTID``,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENTID,
 };
 
 // Initialize Firebase
