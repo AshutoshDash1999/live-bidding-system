4cf61b3d87311833d4b726c80bd1b08492e0c116
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-05-02 16:39:57 +0530

    refactor: update product fallback image and and show preview

--- src/app/publishNewItem/page.tsx
+++ src/app/publishNewItem/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import Button from "@/components/Button";
 import Input from "@/components/Input";
+import { XCircleIcon } from "@heroicons/react/24/outline";
 import Lottie from "lottie-react";
+import Image from "next/image";
 import { ChangeEvent, useRef, useState } from "react";
 
 const imageUploadLottie = require("/public/lottie/image-upload.json");
@@ -15,6 +18,7 @@ const PublishItem = () => {
     description: "",
     auctionEndingDateTime: "",
   });
+  const [selectedImage, setSelectedImage] = useState<any>(null);
 
   const productDetailsChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
     setProductDetails({
@@ -23,22 +27,57 @@ const PublishItem = () => {
     });
   };
 
+  const selectedImageRemoveHandler = () => {
+    setSelectedImage(null);
+  };
+
+  const uploadImageHandler = () => {
+    if (!selectedImage) {
+      imageUploadInputRef?.current?.click();
+    }
+  };
+
+  const selectImageHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setSelectedImage(URL.createObjectURL(e?.target?.files[0] as Blob));
+    }
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-semibold">Publish a New Item</h1>
       <div className="flex flex-row justify-center p-10 gap-20">
         <div className="">
-          <Lottie
-            animationData={imageUploadLottie}
-            className="flex justify-center items-center h-96"
-            loop={true}
-            onClick={() => imageUploadInputRef?.current?.click()}
-          />
+          <div onClick={uploadImageHandler} className="p-4 cursor-pointer">
+            {selectedImage ? (
+              <div className=" relative">
+                <XCircleIcon
+                  className="h-10 absolute text-purple-600 bg-[#ffffffaa] p-1 m-1 rounded-full right-0"
+                  onClick={selectedImageRemoveHandler}
+                />
+                <Image
+                  src={selectedImage}
+                  alt=""
+                  width={300}
+                  height={100}
+                  className="h-96 rounded-lg"
+                />
+              </div>
+            ) : (
+              <Lottie
+                animationData={imageUploadLottie}
+                className="flex justify-center items-center h-96"
+                loop={true}
+              />
+            )}
+          </div>
+
           <input
             className="hidden"
             type="file"
             accept="image/png, image/jpeg, image/jpg"
             ref={imageUploadInputRef}
+            onChange={selectImageHandler}
           />
         </div>
         <div className="flex flex-col gap-2 w-1/3">
@@ -67,6 +106,8 @@ const PublishItem = () => {
             name="auctionEndingDateTime"
             onChange={productDetailsChangeHandler}
           />
+
+          <Button onClick={() => {}}>Publish Item</Button>
         </div>
       </div>
     </div>
