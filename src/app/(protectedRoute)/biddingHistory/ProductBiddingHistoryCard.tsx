352cb6af3a6ec8920ddf516833bda1af1d3ad834
Ashutosh Dash <dashashutosh1999@gmail.com>
2024-05-08 22:43:55 +0530

    feat: navigate to product from history card

--- src/app/(protectedRoute)/biddingHistory/ProductBiddingHistoryCard.tsx
+++ src/app/(protectedRoute)/biddingHistory/ProductBiddingHistoryCard.tsx
@@ -4,6 +4,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import { doc } from "firebase/firestore";
 import Image from "next/image";
+import Link from "next/link";
 import { useDocument } from "react-firebase-hooks/firestore";
 
 dayjs.extend(relativeTime);
@@ -30,7 +31,6 @@ const ProductBiddingHistoryCard = ({
   console.log("productDetails", productDetails?.data());
 
   //   TODO :
-  // 2. navigate to product
   // 3. bidding status: ongoing,lost,  won
 
   if (productDetailsLoading) {
@@ -40,33 +40,43 @@ const ProductBiddingHistoryCard = ({
   }
 
   return (
-    <div className="mb-4 rounded-lg shadow-md hover:shadow-lg transition-all cursor-pointer flex gap-8 p-2">
-      <Image
-        src={productDetails?.data()?.productImageURL}
-        alt={productDetails?.data()?.name}
-        height={300}
-        width={150}
-        className="rounded-lg h-24 w-24 object-cover"
-      />
-      <div className="flex gap-8 justify-between flex-1">
-        <div>
-          <h3 className="font-bold text-4xl">{productDetails?.data()?.name}</h3>
-          <h4>
-            Current price: <span>₹ {productDetails?.data()?.price}</span>
-          </h4>
-          <h2>
-            Auction ending{" "}
-            <span className="font-bold">
-              {dayjs(productDetails?.data()?.auctionEndingDateTime)?.fromNow()}.
-            </span>
-          </h2>
-        </div>
-        <div>
-          <h4>Your bid:</h4>
-          <h2 className="font-bold text-4xl">₹ {biddingData?.bidAmount}</h2>
+    <Link href={`product/${biddingData?.productId}`}>
+      <div className="mb-4 rounded-lg shadow-md hover:shadow-lg transition-all cursor-pointer flex gap-8 p-2">
+        <Image
+          src={productDetails?.data()?.productImageURL}
+          alt={productDetails?.data()?.name}
+          height={300}
+          width={150}
+          className="rounded-lg h-24 w-24 object-cover"
+        />
+        <div className="flex gap-8 justify-between flex-1">
+          <div>
+            <h3 className="font-bold text-4xl">
+              {productDetails?.data()?.name}
+            </h3>
+            <h4 className="text-lg">
+              Current price:{" "}
+              <span className="font-bold">
+                ₹ {productDetails?.data()?.price}
+              </span>
+            </h4>
+            <h2 className="text-lg">
+              Auction ending{" "}
+              <span className="font-bold">
+                {dayjs(
+                  productDetails?.data()?.auctionEndingDateTime
+                )?.fromNow()}
+                .
+              </span>
+            </h2>
+          </div>
+          <div>
+            <h4 className="text-lg">Your bid:</h4>
+            <h2 className="font-bold text-4xl">₹ {biddingData?.bidAmount}</h2>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 export default ProductBiddingHistoryCard;
